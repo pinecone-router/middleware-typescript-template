1a51d89244af753038decc00ba4a659e42d99185
Rafik El Hadi Houari <rehhouari@gmail.com>
2021-05-31 12:46:09 +0100

    update

--- README.md
+++ README.md
@@ -87,7 +87,7 @@ function router() {
 
 | Version | Pinecone Router Versions |
 | ------- | ------------------------ |
-| 0.0.0   | 0.3.0                    |
+| 0.0.0   | ^1.0.0                   |
 
 ## Contributing:
 
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-const PineconeRouterMiddleware = {
+import type {Middleware} from 'pinecone-router/dist/utils'
+
+const PineconeRouterMiddleware: Middleware = {
 	/**
 	 * @property {string} version the version of this middleware.
 	 */
@@ -15,42 +17,31 @@ const PineconeRouterMiddleware = {
 	/**
 	 * This will be called at router initialization.
 	 * used for detecting router settings.
-	 * @param {object} component the router's alpine component.
 	 */
 	init(component, settings) {
 		//load settings
 		this.settings = {
 			...this.settings,
-			...(settings?.middlewares?.[this.name] ?? {}),
+			...(settings.middlewares?.[this.name] ?? {}),
 		};
 	},
 
 	/**
 	 * Called for each route during initialization,
 	 * before the route is processed & added.
-	 * @param {Element} el the route's <template> element
-	 * @param {object} component the router's alpine component
-	 * @param {string} path the route's path
 	 */
 	onBeforeRouteProcessed(el, component, path) {},
 
 	/**
 	 * Will be called before the handlers are executed.
 	 * during navigation (PineconeRouter.navigate()).
-	 * @param {object} route the matched route, undefined if not found.
-	 * @param {string} path the path visited by the client
-	 * @param {boolean} firstload first page load and not link navigation request
-	 * @returns {string|null} 'stop' to make the navigate function exit (make sure to send the loadend event); none to continute execution.
+	 * 
 	 */
 	onBeforeHandlersExecuted(route, path, firstload) {},
 
 	/**
 	 * Will be called after the handlers are executed and done.
 	 * during navigation (PineconeRouter.navigate()).
-	 * @param {object} route the matched route, undefined if not found.
-	 * @param {string} path the path visited by the client
-	 * @param {boolean} firstload first page load and not link navigation request
-	 * @returns {string|null} 'stop' to make the navigate function exit (make sure to send the loadend event); none to continute execution.
 	 */
 	onHandlersExecuted(route, path, firstload) {},
 };
